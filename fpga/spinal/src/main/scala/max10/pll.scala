bf3f02cbdb64f9bc6ff425d605eda6c0340d5860
Tom Verbeure <hombre+github@gmail.com>
2021-01-18 10:28:44 -0800

    Reduce RTL to Spdif example only.

--- fpga/spinal/src/main/scala/max10/pll.scala
+++ fpga/spinal/src/main/scala/max10/pll.scala
@@ -7,9 +7,6 @@ class pll() extends BlackBox{
 
 	val inclk0            = in(Bool)
 	val c0                = out(Bool)
-	val c1                = out(Bool)
-	val c2                = out(Bool)
-	val c3                = out(Bool)
 
     setDefinitionName("pll")
 }
